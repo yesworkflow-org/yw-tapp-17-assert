53dbb29dc2e8e9b4f81fd39b20658ae7bd0291be
Timothy McPhillips <tmcphillips@absoluteflow.org>
2017-03-31 23:32:47 -0700

    Added simple views of YW model.

--- examples/simulate_data_collection/rules/materialize_simple_views.sh
+++ examples/simulate_data_collection/rules/materialize_simple_views.sh
@@ -11,7 +11,25 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
 %set_prolog_flag(unknown, fail).
 
-rule_banner('input(WorkflowInput).').
-printall(input(_)).
+rule_banner('data(Data)').
+printall(data(_)).
+
+rule_banner('step(Step)').
+printall(step(_)).
+
+rule_banner('workflow_in(Data)').
+printall(workflow_in(_)).
+
+rule_banner('workflow_out(Data)').
+printall(workflow_out(_)).
+
+rule_banner('step_in(Data, Step)').
+printall(step_in(_, _)).
+
+rule_banner('step_out(Step, Data)').
+printall(step_out(_, _)).
+
+rule_banner('flow(From, To)').
+printall(flow(_, _)).
 
 END_XSB_STDIN
